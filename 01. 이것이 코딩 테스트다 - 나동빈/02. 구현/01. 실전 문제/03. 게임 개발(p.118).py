82e154074d5b27bc441ef1ebbce18aaad69cb660
p65454 <106226135+p65454@users.noreply.github.com>
2023-03-13 20:57:36 +0900

    230313

--- 01. 이것이 코딩 테스트다 - 나동빈/02. 구현/01. 실전 문제/03. 게임 개발(p.118).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/02. 구현/01. 실전 문제/03. 게임 개발(p.118).py
@@ -5,27 +5,27 @@ for _ in range(n):
     board.append(list(map(int, input().split())))
 
 result = 1
-move = [(-1, 0), (0, -1), (1, 0), (0, 1)]  # 리스트의 0:북쪽, 3:서쪽, 2:남쪽, 1:동쪽
-cur_pos = [(a,b)]
-memory = d
-
-
+move = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # 리스트의 0:북쪽, 1:동쪽, 2:남쪽, 3:서쪽
+cur_pos = [(a, b)]
 check = 0
-next_pos = board[a+move[d][0]][b+move[d][1]]
+
 #현재 방향부터 반시계 방향으로 돌아가면서 움직 일 수 있는지 체크
 while True:
     for _ in range(4):
-        print(a,b,result,cur_pos)
+
         if d == 0:
             d = 3
         else:
             d -= 1
 
-        if (next_pos == 0) and (next_pos not in cur_pos):  #해당 방향의 다음 칸이 육지이면서, 가보지 않은 칸일 때
+        next_pos = (a + move[d][0], b + move[d][1])
+
+        if (board[a+move[d][0]][b+move[d][1]] == 0) and (next_pos not in cur_pos):  #해당 방향의 다음 칸이 육지이면서, 가보지 않은 칸일 때
             a += move[d][0]
             b += move[d][1]
             result += 1
             cur_pos.append((a, b))
+            check = 0
             break
         else:                                               #갈 수 없을 때
             check += 1
@@ -34,6 +34,7 @@ while True:
         if board[a-move[d][0]][b-move[d][1]] == 0:   # 뒤로 갈 곳이 육지라면
             a -= move[d][0]
             b -= move[d][1]
+            check = 0
             if (a, b) not in cur_pos:                #
                 cur_pos.append((a, b))
                 result += 1

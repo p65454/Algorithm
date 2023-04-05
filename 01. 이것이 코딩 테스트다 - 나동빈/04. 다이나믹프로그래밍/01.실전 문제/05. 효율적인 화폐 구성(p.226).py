b7b2c17fc49447f1c24a99677778b5d8c10513af
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-05 20:40:34 +0900

    230405

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/01.실전 문제/05. 효율적인 화폐 구성(p.226).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/01.실전 문제/05. 효율적인 화폐 구성(p.226).py
@@ -5,3 +5,16 @@ for _ in range(n):
     money.append(int(input()))
 
 print(money)
+
+d = [10001] * (m +1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(money[i], m+1):
+        if d[j - money[i]] != 10001:
+            d[j] = min(d[j], d[j - money[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])

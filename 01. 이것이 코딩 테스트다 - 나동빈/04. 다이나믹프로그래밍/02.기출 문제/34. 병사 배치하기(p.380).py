a9c34a5000fd17262d5b4a038e70f59d1ef847e8
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-09 16:01:27 +0900

    230409

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
@@ -1,4 +1,14 @@
+# 34. 병사 배치하기(p.380)
+# www.acmicpc.net/problem/18353
 n = int(input())
 array = list(map(int, input().split()))
+result = 0
 
+for i in range(n-1):
+    if array[i] < array[i+1]:
+        result += 1
 
+if result == 0:
+    result = 1
+
+print(result)

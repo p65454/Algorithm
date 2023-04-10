89972da287efb39cfb092e652c3ea35e8333c6f0
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-10 14:49:10 +0900

    230410

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
@@ -11,15 +11,41 @@ result = 0
 # if result == 0:
 #     result = 1
 d = [0] * (n+1)
-temp = 0
-for i in range(0, n-1):
-    if array[i] < array[i+1]:
-        d[i+1] = max(d[i], temp)
-        for j in range(i+1, n-1):
-            if array[j] > array[j+1]:
-                temp += 1
-    else:
-        d[i] += 1
+d[0] = 1
+x = 0
 
-print(n - d[n])
+while x < n-1:
+
+    temp = 0
+    if array[x] > array[x+1]:
+        d[x+1] = d[x] + 1
+        x += 1
+    elif array[x] < array[x+1]:
+
+        for j in range(x+1, n-1):
+
+            if array[j] < array[j+1]:
+                x += 1
+                temp += 1
+                break
+            elif array[j] > array[j+1]:
+                x += 1
+                temp += 1
+                if j == n-2:
+                    x += 1
+                    temp += 1
+
+                if array[j+1] < array[j-temp]:
+                    break
+            else:
+                x += 1
+            print(x, temp)
+        print(1234)
+        d[x] = max(d[x-temp], temp)
+
+    else:
+        d[x+1] = d[x]
+        x += 1
+print(d)
+print(n - d[n-1])
 

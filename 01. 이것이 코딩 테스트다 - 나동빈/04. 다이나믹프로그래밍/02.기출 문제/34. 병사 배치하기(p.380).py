f6b289298d66e3a488b3b41710c0f9513ce00162
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-10 13:13:14 +0900

    230410

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/34. 병사 배치하기(p.380).py
@@ -4,11 +4,22 @@ n = int(input())
 array = list(map(int, input().split()))
 result = 0
 
-for i in range(n-1):
+# for i in range(n-1):
+#     if array[i] < array[i+1]:
+#         result += 1
+#
+# if result == 0:
+#     result = 1
+d = [0] * (n+1)
+temp = 0
+for i in range(0, n-1):
     if array[i] < array[i+1]:
-        result += 1
+        d[i+1] = max(d[i], temp)
+        for j in range(i+1, n-1):
+            if array[j] > array[j+1]:
+                temp += 1
+    else:
+        d[i] += 1
 
-if result == 0:
-    result = 1
+print(n - d[n])
 
-print(result)

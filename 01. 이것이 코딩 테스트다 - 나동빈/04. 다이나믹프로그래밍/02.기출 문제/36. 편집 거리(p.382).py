1d09cc2d150cf6f485d8497aaf6bf6a7895ed356
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-14 14:46:11 +0900

    230414

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/36. 편집 거리(p.382).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/36. 편집 거리(p.382).py
@@ -1,18 +1,4 @@
-a = list(input())
-b = list(input())
-temp = [a[0]]
-print(a, b)
-
-dp = [0] * (len(b) + 1)
-for i in range(len(b)):
-    if temp[i] == b[i]:
-        temp.append(b[i])
-        dp[i] = 0
-    else:
-        if temp[i] not in b[i]:
-
-            temp.pop(i)
-            temp.insert(i, b[i])
-        else:
-            temp.insert(i, b[i])
+def edit_dist(str1, str2):
+    n = len(str1)
+    m = len(str2)
 

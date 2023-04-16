1afcde08da58b6c13fc83c345991ec70505af77a
p65454 <106226135+p65454@users.noreply.github.com>
2023-04-16 18:46:32 +0900

    230416

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/36. 편집 거리(p.382).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹프로그래밍/02.기출 문제/36. 편집 거리(p.382).py
@@ -1,4 +1,2 @@
-def edit_dist(str1, str2):
-    n = len(str1)
-    m = len(str2)
+
 
---
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS_BFS/01.실전 문제/03. 음료수 얼려 먹기(p.149).py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+icebox = []
+for _ in range(n):
+    icebox.append(list(map(int, input())))
+
+print(icebox)
+def dfs(x, y):
+    if x < 0 or y < 0 or x > n-1 or y > m-1:
+        return False
+    if icebox[x][y] == 0:
+        icebox[x][y] = 1
+        dfs(x, y+1)
+        dfs(x, y-1)
+        dfs(x-1, y)
+        dfs(x+1, y)
+
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:  """어차피 처음으로 붙어있는 아이스크림 덩어리를 만나는 순간 재귀로 들어가서 
+                                    덩어리째로 다 1이 들어가고 True는 한번만 반환하기 때문에, 아이스크림 덩어리당 result에 1씩 더해진다"""
+            result += 1
+print(result)
+

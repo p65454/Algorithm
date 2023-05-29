bb7cc01d187ebb013377ae85588fdec68dd70106
p65454 <106226135+p65454@users.noreply.github.com>
2023-05-29 16:51:46 +0900

    230529

--- 01. 이것이 코딩 테스트다 - 나동빈/05. DFS_BFS/02.기출 문제/20. 감시 피하기(p.351).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS_BFS/02.기출 문제/20. 감시 피하기(p.351).py
@@ -1,4 +1,4 @@
-#https://www.acmicpc.net/problem/18428
+# https://www.acmicpc.net/problem/18428
 import itertools, copy
 import sys
 from itertools import combinations
@@ -11,38 +11,104 @@ for i in range(n):
 s = []
 t = []
 x = []
-dx = [0, 0, -1, 1]  # 우 좌 하 상
-dy = [1, -1, 0, 0]
-array_proto = copy.deepcopy(array)
+
+#array_copy = copy.deepcopy(array)
 for i in range(n):
     for j in range(n):
         if array[i][j] == 'S':
             s.append((i,j))
-        if array[i][j] == 'T':
+        elif array[i][j] == 'T':
             t.append((i,j))
         else:
             x.append((i,j))
-
 num_s = len(s)
-
+print(num_s)
+print(s)
 # 조합으로 장애물 3개 위치 설정
 def comb_wall(wall):
     result = list(itertools.combinations(wall, 3))
     return result
 
-def spray_T(t, array_proto, wall):
-    cnt = 0
-    for i in range(len(t)):
-        x = t[i][0]
-        y = t[i][1]
-        for j in range(n):
-            while x - cnt > 0:
-
-                array_proto[x][j] = 'T'
-                array_proto[j][y] = 'T'
-    return array_proto
-
+cnt = 1
 wall = comb_wall(x)
+def run_T(t, array, wall):
+    count_s_list = []
+    for j in range(len(wall)):
+        array_proto = copy.deepcopy(array)
+        for k in range(3):
+            a = wall[j][k][0]
+            b = wall[j][k][1]
+            array_proto[a][b] = 'O'
+        for i in range(len(t)):
+            x = t[i][0]
+            y = t[i][1]
+            temp = 0
+
+            while True: #상
+                nx, ny = x - (1 + temp), y
+                if 0 <= nx < n and 0 <= ny < n and array_proto[nx][ny] != 'O' and array_proto[nx][ny] != 'T':
+                    array_proto[nx][ny] = 'A'
+                    temp += 1
+                else:
+                    temp = 0
+                    break
+            while True: #하
+                nx, ny = x + (1 + temp), y
+                if 0 <= nx < n and 0 <= ny < n and array_proto[nx][ny] != 'O' and array_proto[nx][ny] != 'T':
+                    array_proto[nx][ny] = 'B'
+                    temp += 1
+                else:
+                    temp = 0
+                    break
+            while True: #좌
+                nx, ny = x, y - (1 + temp)
+                if 0 <= nx < n and 0 <= ny < n and array_proto[nx][ny] != 'O' and array_proto[nx][ny] != 'T':
+                    array_proto[nx][ny] = 'C'
+                    temp += 1
+                else:
+                    temp = 0
+                    break
+            while True: #우
+                nx, ny = x, y + (1 + temp)
+                if 0 <= nx < n and 0 <= ny < n and array_proto[nx][ny] != 'O' and array_proto[nx][ny] != 'T':
+                    array_proto[nx][ny] = 'D'
+                    temp += 1
+                else:
+                    temp = 0
+                    break
+        for b in range(len(array_proto)):
+            print(array_proto[b])
+        print('---------------------------------')
+        count_s = 0
+        for a in range(len(array_proto)):
+            count_s += array_proto[a].count('S')
+        count_s_list.append(count_s)
+    return count_s_list
+
+result = run_T(t, array, wall)
+print(result)
+if num_s in result:
+    print('YES')
+else:
+    print('NO')
+
+'''
+5
+X S X X T
+T X S X X
+X X X X X
+X T X X X
+X X T X X
+# YES
+
+4
+S S S T
+X X X X
+X X X X
+T T T X
+# NO
+
+'''
 
 
 

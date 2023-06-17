e0913fdbf862fbd1bc10f3141df7a3ff6ad9c6fc
p65454 <106226135+p65454@users.noreply.github.com>
2023-06-17 15:42:40 +0900

    230617

--- 01. 이것이 코딩 테스트다 - 나동빈/06. 이진 탐색/02. 기출 문제/28. 고정점 찾기(p.368).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/06. 이진 탐색/02. 기출 문제/28. 고정점 찾기(p.368).py
@@ -12,7 +12,19 @@ def BTS(array, start, end):
     else:
         return BTS(array, start, mid - 1)
     return
+
 start = 0
 end = len(array) - 1
-
 print(BTS(array, start, end))
+
+# 5
+# -15 -6 1 3 7
+# res = 3
+
+# 7
+# -15 -4 2 8 9 13 15
+# res = 2
+
+# 7
+# -15 -4 3 8 9 13 15
+# res = -1
